013b077518a15fbb238e66843547e13d847b8839
resteve <resteve@zikzakmedia.com>
2015-12-01 10:57:28 +0100

    Tests

--- tests/test_account_invoice_merger.py
+++ tests/test_account_invoice_merger.py
@@ -1,46 +1,18 @@
-#!/usr/bin/env python
 # This file is part of the account_invoice_merger module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends, test_view
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoiceMergerTestCase(unittest.TestCase):
-    '''
-    Test Account Invoice Merger module.
-    '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_invoice_merger')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('account_invoice')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+class AccountInvoiceMergerTestCase(ModuleTestCase):
+    'Test Account Invoice Merger module'
+    module = 'account_invoice_merger'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            AccountInvoiceMergerTestCase))
+        AccountInvoiceMergerTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
