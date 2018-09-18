f859a9d24d7f3f16462934b96c59ea3026153879
Raimon Esteve <raimon@nan-tic.com>
2018-09-18 17:11:50 +0200

    Register classes and migrate to py3

--- __init__.py
+++ __init__.py
@@ -1,15 +1,14 @@
 # This file is part of the account_invoice_merger module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 from trytond.pool import Pool
-from .invoice import *
+from . import invoice
 
 
 def register():
     Pool.register(
-        InvoiceMergeStart,
+        invoice.InvoiceMergeStart,
         module='account_invoice_merger', type_='model')
     Pool.register(
-        InvoiceMerge,
+        invoice.InvoiceMerge,
         module='account_invoice_merger', type_='wizard')
