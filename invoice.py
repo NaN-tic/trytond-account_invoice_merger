a904a0b9c0aee3a48aefd8fa1131bee13e8ac41b
Raimon Esteve <raimonesteve@gmail.com>
2020-10-27 10:49:13 +0100

    Upgrade and improve to 5.4

--- invoice.py
+++ invoice.py
@@ -5,6 +5,8 @@ from trytond.model import ModelView, fields
 from trytond.pool import Pool
 from trytond.transaction import Transaction
 from trytond.wizard import Button, StateAction, StateView, Wizard
+from trytond.i18n import gettext
+from trytond.exceptions import UserError
 
 __all__ = ['InvoiceMerge', 'InvoiceMergeStart']
 
@@ -25,24 +27,6 @@ class InvoiceMerge(Wizard):
             ])
     merge = StateAction('account_invoice.act_invoice_form')
 
-    @classmethod
-    def __setup__(cls):
-        super(InvoiceMerge, cls).__setup__()
-        cls._error_messages.update({
-                'state_not_draft': ('You can not merge the invoice %s because '
-                    'its state is different from draft.'),
-                'different_parties': ('You can not merge these invoices '
-                    'because their customers/suppliers are different.'),
-                'different_parties_address': ('You can not merge these '
-                    'invoices because their invoice addresses are different.'),
-                'different_journals': ('You can not merge these invoices '
-                    'because their journals are different.'),
-                'different_payment_term': ('You can not merge these invoices '
-                    'because their payment terms are different.'),
-                'different_bank_account': ('You can not merge these invoices '
-                    'because their bank accounts are different.'),
-                })
-
     def default_start(self, fields):
         return {
             'invoices': ', '.join([str(id)
@@ -55,73 +39,67 @@ class InvoiceMerge(Wizard):
         InvoiceLine = pool.get('account.invoice.line')
 
         invoices = Invoice.browse(Transaction().context['active_ids'])
-        new_invoice = False
-        vals = {}
-        description = ''
 
+        parties = set()
+        types = set()
+        invoice_address = set()
+        journals = set()
+        payment_terms = set()
+        bank_accounts = set()
+        descriptions = []
+        references = []
         for invoice in invoices:
             if invoice.state != 'draft':
-                self.raise_user_error('state_not_draft',
-                    (invoice.rec_name,))
+                raise UserError(gettext('account_invoice_merger.msg_state_not_draft',
+                    invoice=invoice.rec_name))
+            if invoice.number:
+                raise UserError(gettext('account_invoice_merger.msg_has_number',
+                    invoice=invoice.rec_name))
 
-            if 'party' not in vals:
-                vals['party'] = invoice.party
-            elif vals['party'] != invoice.party:
-                self.raise_user_error('different_parties',
-                    (invoice.rec_name,))
-
-            if 'invoice_address' not in vals:
-                vals['invoice_address'] = invoice.invoice_address
-            elif vals['invoice_address'] != invoice.invoice_address:
-                self.raise_user_error('different_parties_address',
-                    (invoice.rec_name,))
-
-            if 'journal' not in vals:
-                vals['journal'] = invoice.journal
-            elif vals['journal'] != invoice.journal:
-                self.raise_user_error('different_journals',
-                    (invoice.rec_name,))
-
-            if 'payment_term' not in vals:
-                vals['payment_term'] = invoice.payment_term
-            elif vals['payment_term'] != invoice.payment_term:
-                self.raise_user_error('different_payment_term',
-                    (invoice.rec_name,))
-            
+            types.add(invoice.type)
+            parties.add(invoice.party)
+            invoice_address.add(invoice.invoice_address)
+            journals.add(invoice.journal)
+            payment_terms.add(invoice.payment_term)
             bank_account = getattr(invoice, 'bank_account', None)
-            if 'bank_account' not in vals:
-                vals['bank_account'] = bank_account
-            elif vals['bank_account'] != bank_account:
-                self.raise_user_error('different_bank_account',
-                                      (invoice.rec_name,))
+            if bank_account:
+                bank_accounts.add(bank_account)
+            descriptions.append(invoice.description)
+            references.append(invoice.reference)
 
-            description += '%s ' % invoice.description
+        if len(types) > 1:
+            raise UserError(gettext('account_invoice_merger.msg_different_types',
+                invoices=', '.join([str(invoice.id) for invoice in invoices])))
+        elif len(parties) > 1:
+            raise UserError(gettext('account_invoice_merger.msg_different_parties',
+                invoices=', '.join([str(invoice.id) for invoice in invoices])))
+        elif len(invoice_address) > 1:
+            raise UserError(gettext('account_invoice_merger.msg_different_parties_address',
+                invoices=', '.join([str(invoice.id) for invoice in invoices])))
+        elif len(journals) > 1:
+            raise UserError(gettext('account_invoice_merger.msg_different_journals',
+                invoices=', '.join([str(invoice.id) for invoice in invoices])))
+        elif len(payment_terms) > 1:
+            raise UserError(gettext('account_invoice_merger.msg_different_payment_term',
+                invoices=', '.join([str(invoice.id) for invoice in invoices])))
+        elif len(bank_accounts) > 1:
+            raise UserError(gettext('account_invoice_merger.msg_different_bank_account',
+                invoices=', '.join([str(invoice.id) for invoice in invoices])))
 
-            if not new_invoice:
-                default = {
-                    'lines': None,
-                    'taxes': None,
-                    'bank_account': None,
-                    'type': invoice.type,
-                    }
-                new_invoice = Invoice.copy([invoice],
-                    default=default)[0]
+        main_invoice, = invoices[:1]
+        other_invoices = invoices[1:]
 
-            if invoice.lines:
-                InvoiceLine.write([line for line in invoice.lines],
-                        {'invoice': new_invoice})
-
-        to_write = {'state': 'cancel'}
-        if vals['bank_account']:
-            to_write['bank_account'] = None
-            new_invoice.bank_account = vals['bank_account']
-            new_invoice.save()
+        invoice.description = ', '.join(descriptions)
+        invoice.reference = ', '.join(descriptions)
+        InvoiceLine.write([line for i in other_invoices for line in i.lines],
+                {'invoice': main_invoice})
 
         with Transaction().set_user(0, set_context=True):
-            Invoice.update_taxes([new_invoice])
-            Invoice.write(invoices, to_write)
-            Invoice.delete(invoices)
+            Invoice.update_taxes([main_invoice])
+            Invoice.cancel(other_invoices)
+            # TODO sao can not reload records
+            # Invoice.delete(other_invoices)
 
-        data = {'res_id': [new_invoice.id]}
+        data = {'res_id': [main_invoice.id]}
         action['views'].reverse()
         return action, data
--- tests/test_account_invoice_merger.py
+++ tests/test_account_invoice_merger.py
@@ -2,8 +2,11 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class AccountInvoiceMergerTestCase(ModuleTestCase):
@@ -15,4 +18,9 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountInvoiceMergerTestCase))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_invoice_merger.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
